947233b36f706b0bcf68bc0577ced4ffa60ef8ee
sho naito <sho20100@gmail.com>
2020-04-21 15:41:16 +0900

    Implement

--- function.go
+++ function.go
@@ -1,26 +1,157 @@
 // Package p contains an HTTP Cloud Function.
 package p
 
+// package main
+
 import (
 	"encoding/json"
 	"fmt"
-	"html"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/slack-go/slack"
 )
+
+type gitHubAlertPayload struct {
+	ID                  int    `json:"id"`
+	AffectedRange       string `json:"affected_range"`
+	AffectedPackageName string `json:"affected_package_name"`
+	ExternalReference   string `json:"external_reference"`
+	ExternalIdentifier  string `json:"external_identifier"`
+}
+
+type gitHubRepositoryPayload struct {
+	FullName string `json:"full_name"`
+}
+
+type githubPayload struct {
+	Alert      gitHubAlertPayload      `json:"alert"`
+	Repository gitHubRepositoryPayload `json:"repository"`
+}
+
+func buildAttachment(gh githubPayload) slack.Attachment {
+
+	attachment := slack.Attachment{
+		// Text:          "alert",
+		Color:         "warning",
+		Pretext:       ":warning: Security alert created! :warning:",
+		AuthorName:    "sho2010",
+		AuthorSubname: "sho2010",
+		Title:         "Security alert(CVE link)",
+		TitleLink:     gh.Alert.ExternalReference,
+		Ts:            json.Number(strconv.FormatInt(time.Now().Unix(), 10)),
+	}
+	repository := slack.AttachmentField{
+		Title: "Repository",
+		Value: gh.Repository.FullName,
+		Short: true,
+	}
+
+	id := slack.AttachmentField{
+		Title: "Identifier",
+		Value: gh.Alert.ExternalIdentifier,
+		Short: true,
+	}
+
+	pkg := slack.AttachmentField{
+		Title: "Affected Package Name",
+		Value: gh.Alert.AffectedPackageName,
+		Short: true,
+	}
+
+	version := slack.AttachmentField{
+		Title: "Affected Range",
+		Value: gh.Alert.AffectedRange,
+		Short: true,
+	}
+
+	attachment.Fields = []slack.AttachmentField{
+		repository,
+		id,
+		pkg,
+		version,
+	}
+
+	return attachment
+}
+
+func buildSlackPayload(gh githubPayload) slack.Message {
+
+	// Header Section
+	headerText := slack.NewTextBlockObject("mrkdwn", ":warning: Security alert created!", true, false)
+	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+
+	// Fields
+	repo := fmt.Sprintf("*Repository:*\n`%s`", gh.Repository.FullName)
+	repositoryField := slack.NewTextBlockObject("mrkdwn", repo, false, false)
+
+	id := fmt.Sprintf("*Identifier:*\n<%s|%s>", gh.Alert.ExternalReference, gh.Alert.ExternalIdentifier)
+	identifierField := slack.NewTextBlockObject("mrkdwn", id, false, false)
+
+	pkg := fmt.Sprintf("*Affected Package Name:*\n`%s`", gh.Alert.AffectedPackageName)
+	packageField := slack.NewTextBlockObject("mrkdwn", pkg, false, false)
+
+	version := fmt.Sprintf("*Affected Range:*\n`%s`", gh.Alert.AffectedRange)
+	rangeField := slack.NewTextBlockObject("mrkdwn", version, false, false)
+
+	fieldSlice := make([]*slack.TextBlockObject, 0)
+	fieldSlice = append(fieldSlice, repositoryField)
+	fieldSlice = append(fieldSlice, identifierField)
+	fieldSlice = append(fieldSlice, packageField)
+	fieldSlice = append(fieldSlice, rangeField)
+
+	fieldsSection := slack.NewSectionBlock(nil, fieldSlice, nil)
+
+	msg := slack.NewBlockMessage(
+		headerSection,
+		fieldsSection,
+	)
+	return msg
+}
 
 // Github2Slack prints the JSON encoded "message" field in the body
 // of the request or "Hello, World!" if there isn't one.
 func Github2Slack(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Message string `json:"message"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World!")
+	var payload githubPayload
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		fmt.Fprint(w, "Payload decode error")
 		return
 	}
-	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
+
+	// block対応したらこっち使う
+	// buildSlackPayload(payload)
+
+	msg := slack.WebhookMessage{
+		Attachments: []slack.Attachment{buildAttachment(payload)},
+	}
+
+	err := slack.PostWebhook(os.Getenv("WEBHOOK_URL"), &msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	var payload githubPayload
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Println("err")
 		return
 	}
-	fmt.Fprint(w, html.EscapeString(d.Message))
+	json.Unmarshal(data, &payload)
+
+	// block対応したらこっち使う
+	// buildSlackPayload(payload)
+
+	msg := slack.WebhookMessage{
+		Attachments: []slack.Attachment{buildAttachment(payload)},
+	}
+
+	err = slack.PostWebhook(os.Getenv("WEBHOOK_URL"), &msg)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/sho2010/github-security-alert-notify
 
 go 1.13
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pkg/errors v0.8.0
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/slack-go/slack v0.6.4
+)
